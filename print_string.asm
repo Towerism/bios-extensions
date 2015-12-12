a6944919fd9d7902330eb1300c03a6fd2e070ca0
Martin Fracker <martin.frackerjr@gmail.com>
2015-12-12 00:35:33 -0600

    Add comments to print_string

--- print_string.asm
+++ print_string.asm
@@ -2,14 +2,14 @@
 ;; bx must point to a null terminated string
 print_string:
   pusha
-  mov ah, 0xe
+  mov ah, 0xe                   ; BIOS scrolling tele-type output
 
 print_string_loop:
-  mov al, [bx]
-  cmp al, 0x0
-  je print_string_done
-  int 0x10
-  add bx, 0x1
+  mov al, [bx]                  ; Store character in al
+  cmp al, 0x0                   ; check if character is the nullbyte
+  je print_string_done          ; if it is we're done
+  int 0x10                      ; otherwise, print the character
+  add bx, 0x1                   ; move onto next character in the string
   jmp print_string_loop
 
 print_string_done:
